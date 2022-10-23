3cac994e0213dca7fdff1b86c9ff7834571e325b
samoore036 <samoore036@gmail.com>
2022-10-22 18:45:55 -0700

    changed code to interact with webpage only along with adding elements to html to interract with

--- main.js
+++ main.js
@@ -1,52 +1,93 @@
 let playerWins = 0;
 let computerWins = 0;
 
+//stores latest clicked choice
+let playerChoice = '';
+
+//on the click of a button, currentChoice variable will equal that button's text content
+let rockBtn = document.getElementById('btn-rock').addEventListener('click', playerSelection);
+let paperBtn = document.getElementById('btn-paper').addEventListener('click', playerSelection);
+let scissorsBtn = document.getElementById('btn-scissors').addEventListener('click', playerSelection);
+
+//playerSelection variable becomes the latest button clicked
+function playerSelection(e) {
+    playerChoice = e.target.textContent;
+}
+
+let list = document.getElementById('results-list');
+let resultsDiv = document.getElementById('results');
+
+//click play to start each round until either the player or computer gets 5 wins
+let playBtn = document.getElementById('btn-play').addEventListener('click', playRound);
+
+function displayResult() {
+    let result = '';
+    if (playerWins == 5) {
+        result = 'Player wins';
+    } else {
+        result = 'Computer wins';
+    }
+    let resultText = document.createTextNode(result);
+    resultsDiv.appendChild(resultText);
+}
+
+function gameOver() {
+    if (playerWins == 5 || computerWins == 5) {
+        return true;
+    }
+    return false;
+}
+
+function playRound() {
+    //only use function if player has selected a choice
+    if (!playerChoice) {
+        return;
+    }
+    let computerChoice = getComputerChoice();
+    let result = '';
+    //all player win conditions
+    if (playerChoice === 'Rock' && computerChoice === 'Scissors'
+    || playerChoice === 'Scissors' && computerChoice === 'Paper'
+    || playerChoice === 'Paper' && computerChoice === 'Rock') {
+        playerWins++;
+        result = `${playerChoice} beats ${computerChoice.toLowerCase()}, player wins.`;
+    }
+    //all computer win conditions
+    if (computerChoice === 'Rock' && playerChoice === 'Scissors'
+    || computerChoice === 'Scissors' && playerChoice === 'Paper'
+    || computerChoice === 'Paper' && playerChoice === 'Rock') {
+        computerWins++;
+        result = `${computerChoice} beats ${playerChoice.toLowerCase()}, computer wins.`;
+    }
+    //draw
+    if (playerChoice === computerChoice) {
+        result = 'Draw.';
+    }
+    addListResult(result);
+
+    //if either player or computer reaches 5 wins, display the result and end the function
+    if (gameOver()) {
+        displayResult();
+        return;
+    }
+    playerChoice = ''; //reset playerChoice after each round
+}
+
+function addListResult(result) {
+    let roundResult = document.createElement('li');
+    roundResult.className = 'round-result';
+    roundResult.textContent = result;
+    list.appendChild(roundResult);
+}
+
+//random choice selected from array 
 function getComputerChoice() {
     const choices = ['Rock', 'Paper', 'Scissors'];
     let randomIndex = Math.floor(Math.random() * 3);
     return choices[randomIndex]; 
 }
 
-function playerSelection() {
-    return prompt();
-}
 
-function playRound(playerSelection, getComputerChoice) {
-    let playerMove = playerSelection().toLowerCase();
-    let computerMove = getComputerChoice().toLowerCase();
-    //all player win conditions
-    if (playerMove === 'rock' && computerMove === 'scissors'
-    || playerMove === 'scissors' && computerMove === 'paper'
-    || playerMove === 'paper' && computerMove === 'rock') {
-        playerWins++;
-        console.log(`${capitalize(playerMove)} beats ${computerMove}, player wins.`);
-    }
-    //all computer win conditions
-    if (computerMove === 'rock' && playerMove === 'scissors'
-    || computerMove === 'scissors' && playerMove === 'paper'
-    || computerMove === 'paper' && playerMove === 'rock') {
-        computerWins++;
-        console.log(`${capitalize(computerMove)} beats ${playerMove}, computer wins.`);
-    }
-    //draw
-    if (playerMove === computerMove) {
-        console.log('Draw.');
-    }
-}
 
-function capitalize(string) {
-    let firstChar = string[0].toUpperCase();
-    return firstChar + string.slice(1).toLowerCase();
-}
 
-function playGame() {
-    console.log('Best out of 5!');
-    for (let i = 0; i < 5; i++) {
-        playRound(playerSelection, getComputerChoice);
-    }
-    playerWins > computerWins ? console.log(`Player wins: ${playerWins} to ${computerWins}`)
-    : computerWins > playerWins ? console.log(`Computer wins: ${computerWins} to ${playerWins}`) 
-    : console.log(`Draw! Player wins: ${playerWins}, Computer wins: ${computerWins}`);
-}
 
-playGame();
